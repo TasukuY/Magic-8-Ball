a369f2968730e43d27d9c81e8a53c61e4363efcc
TasukuY <alohaty81@gmail.com>
2022-03-01 14:57:03 -0700

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Tasuku Yamamoto on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
