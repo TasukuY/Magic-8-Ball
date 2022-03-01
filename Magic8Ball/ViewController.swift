c520c0a2644c8a3354e89918e528d9d9b04afac6
TasukuY <alohaty81@gmail.com>
2022-03-01 16:45:11 -0700

    project done

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -13,7 +13,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    let ballArray = [#imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball1")]
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askPressed(_ sender: UIButton) {
+        let randomNum = Int.random(in: 0...4)
+        imageView.image = ballArray[randomNum]
+    }
+    
+    
+    
 }
 
+  
